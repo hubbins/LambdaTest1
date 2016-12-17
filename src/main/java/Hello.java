1f2a734ebf0c313e3e69725c7d4b2a65e0bb300e
Sean OConnor <soconnor@yahoo.com>
2016-12-16 20:53:27 -0600

    initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+public class Hello {
+    public String myHandler(int myCount, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("received : " + myCount);
+        return "received : " + myCount;
+    }
+}
