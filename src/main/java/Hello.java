e695c5963cb9b7dd7e4e68a39964e938c2c21e0e
Sean OConnor <soconnor@yahoo.com>
2016-12-17 19:55:17 -0600

    log4j

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,15 @@
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
+
 public class Hello {
     public String myHandler(int myCount, Context context) {
-        LambdaLogger logger = context.getLogger();
-        logger.log("received : " + myCount);
-        return "received : " + myCount;
+        LambdaLogger log = context.getLogger();
+        //final Logger log = Logger.getLogger("Hello");
+        String value = System.getenv("test");
+        log.log("env: " + value);
+        log.log("received : " + myCount);
+
+        return "received : " + myCount + " env: " + value;
     }
 }
